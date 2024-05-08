c75b299d56fb2a5d6cce881b3b27f9479095e9d8
Dmytro Chyr <dima543439@gmail.com>
2024-05-08 23:55:38 +0300

    Lab4 task 2

--- app/src/main/java/com/example/kotlin_lab/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_lab/MainActivity.kt
@@ -1,27 +1,22 @@
 package com.example.kotlin_lab
 
 import android.os.Bundle
-import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.kotlin_lab.ui.theme.Kotlin_LabTheme
+import java.util.Locale
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main)
+        setContentView(R.layout.activity_main)
 
-        val ship = findViewById<ImageView>(R.id.shipView)
-        val shipAnim = AnimationUtils.loadAnimation(this, R.anim.ship_anim)
-        ship.startAnimation(shipAnim)
+        val flagLayout = findViewById<LinearLayout>(R.id.flag_layout)
+        val deviceLanguage = Locale.getDefault().language
 
+        if (deviceLanguage == "fr") {
+            layoutInflater.inflate(R.layout.france_flag, flagLayout)
+        }
     }
 }
