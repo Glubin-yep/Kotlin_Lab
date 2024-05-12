855f4ce68a414138a66911d35ecd1e5202827b41
Dmytro Chyr <dima543439@gmail.com>
2024-05-12 19:54:16 +0300

    Lab5 task 3-8

--- app/src/main/java/com/example/kotlin_lab/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_lab/MainActivity.kt
@@ -1,34 +1,65 @@
 package com.example.kotlin_lab
 
-import android.app.TabActivity
-import android.content.Intent
+import android.app.Activity
 import android.os.Bundle
-import android.widget.TabHost.TabSpec
+import android.view.KeyEvent
+import android.view.View
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.Toast
+import android.widget.ToggleButton
 
 
-class MainActivity : TabActivity() {
+class MainActivity : Activity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
-        val res = resources
-        val tab1Indicator = res.getString(R.string.tab1_indicator)
-        val tab2Indicator = res.getString(R.string.tab2_indicator)
-        val tab3Indicator = res.getString(R.string.tab3_indicator)
-        val tabHost = getTabHost()
-        var spec: TabSpec?
-        var intent: Intent
-        intent = Intent().setClass(this, StudentsActivity::class.java)
-        spec = tabHost.newTabSpec("students").setIndicator(tab1Indicator)
-            .setContent(intent)
-        tabHost.addTab(spec)
-        intent = Intent().setClass(this, TeachersActivity::class.java)
-        spec = tabHost.newTabSpec("teachers").setIndicator(tab2Indicator)
-            .setContent(intent)
-        tabHost.addTab(spec)
-        intent = Intent().setClass(this, ClassesActivity::class.java)
-        spec = tabHost.newTabSpec("class").setIndicator(tab3Indicator)
-            .setContent(intent)
-        tabHost.addTab(spec)
-        tabHost.setCurrentTab(1)
+
+        val userName = findViewById<EditText>(R.id.user_name)
+        userName.setOnKeyListener { v, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                Toast.makeText(applicationContext, userName.text, Toast.LENGTH_SHORT).show()
+                return@setOnKeyListener true
+            }
+            false
+        }
     }
+
+    fun onButtonClicked(view: View) {
+        Toast.makeText(this, "Кнопка нажата", Toast.LENGTH_SHORT).show()
+    }
+
+    fun onCheckboxClicked(view: View) {
+        if ((view as CheckBox).isChecked) {
+            Toast.makeText(this, "Відмічено", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(
+                this, "Не відмічено",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+    fun onToggleClicked(view: View) {
+        if ((view as ToggleButton).isChecked) {
+            Toast.makeText(this, "Ввівккнено", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Вимкнено", Toast.LENGTH_SHORT).show()
+        }
+    }
+    fun onRadioButtonClicked(view: View) {
+        val rb = view as RadioButton
+        Toast.makeText(
+            this, "Вибраний звір: " + rb.getText(),
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+
+    fun onClearButtonClicked(view: View) {
+        val userNameEditText = findViewById<EditText>(R.id.user_name)
+        userNameEditText.setText("")
+    }
+
 }
